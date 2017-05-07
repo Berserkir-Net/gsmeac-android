0f8a9586d06e2cc847306533d30b5b3d0ec3863a
Liam Cottle <liam@liamcottle.com>
2017-05-07 16:48:01 +1200

    setup main activity toolbar

--- app/src/main/java/com/liamcottle/gsmeac/activity/MainActivity.java
+++ app/src/main/java/com/liamcottle/gsmeac/activity/MainActivity.java
@@ -1,14 +1,23 @@
 package com.liamcottle.gsmeac.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import com.liamcottle.gsmeac.R;
 
 public class MainActivity extends GSMEACActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+
     }
 
 }
