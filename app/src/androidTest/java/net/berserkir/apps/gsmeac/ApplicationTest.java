59d8dca985b84776cfc91783c59d0241d2a6025d
Dyson Parkes <dyson@berserkir.net>
2023-12-10 17:27:40 +1300

    Refactor for androidx

--- app/src/androidTest/java/net/berserkir/apps/gsmeac/ApplicationTest.java
+++ app/src/androidTest/java/net/berserkir/apps/gsmeac/ApplicationTest.java
@@ -1,13 +1,14 @@
-package com.liamcottle.gsmeac;
+package net.berserkir.apps.gsmeac;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+//import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+/*
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-}
+}*/
